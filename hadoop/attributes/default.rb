3bced1926ef20c3759bf0d7ef4cd1d2c09075c2b
An Binh Tran <an.tran@unswalumni.com>
2014-09-26 10:32:06 +1000

    Update default.rb

--- hadoop/attributes/default.rb
+++ hadoop/attributes/default.rb
@@ -1,8 +1,10 @@
 default[:Hadoop][:Core][:version] = "2.5.1"
 default[:Hadoop][:Core][:hadoopNamenodeDir] = "/home/hduser/hdfs/namenode"
 default[:Hadoop][:Core][:hadoopDatanodeDir] = "/home/hduser/hdfs/datanode"
+default[:Spark][:Core][:version] = "1.1.0-bin-hadoop2.4"
+default[:HBase][:Core][:version] = "0.99.0"
 default[:HBase][:Core][:zookeeperDir] = "/home/hduser/zookeeper"
 default[:HBase][:Core][:zookeeperQuorumSize] = 3
 override[:java][:install_flavor] = "oracle"
 override[:java][:oracle][:accept_oracle_download_terms] = true
-override[:java][:jdk_version] = "7"
+override[:java][:jdk_version] = "8"
